7c9c4bf893695123c9f206f5e64ccc0fe1606c33
AnnaNeiman <annaneimandesigns@gmail.com>
2021-10-18 19:00:37 -0600

    update css & javascript

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,19 @@ function menuToggle() {
     x.className = 'navtoggle';
   }
 }
+
+$('a[href*="#"]:not([href="#"])').click(function() {
+
+   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+       var target = $(this.hash);
+
+
+       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+       if (target.length) {
+           $('html, body').animate({
+               scrollTop: target.offset().top-130
+           }, 1000);
+           return false;
+       }
+   }
+});
